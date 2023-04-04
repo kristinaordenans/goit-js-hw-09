5199ec6e431e13921194ad0162a3fb299d432597
kristinaordenans <okristin92@gmail.com>
2023-04-04 14:14:03 +0300

    3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
+import Notiflix from 'notiflix';
+import throttle from 'lodash.throttle';
+
+const formEl = document.querySelector('.form');
+const submitBtn = document.querySelector('button');
+submitBtn.setAttribute(`disabled`, ``);
+
+formEl.addEventListener(`input`, throttle(checkInputValue, 1000));
+formEl.addEventListener('submit', handleSubmitBnt);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+        if (shouldResolve) {
+        resolve(`Fulfiled ${position} in ${delay} ms`)
+        } else {
+        reject(`Rejected ${position} in ${delay} ms`)
+        }
+    }, delay)
+  })
 }
+
+function checkInputValue() {
+  if (Number(formEl.elements.delay.value) && Number(formEl.elements.step.value) &&  Number(formEl.elements.amount.value)){
+    submitBtn.removeAttribute("disabled");
+  } 
+}
+
+function handleSubmitBnt(e){
+    e.preventDefault();
+  let delay = Number(formEl.elements.delay.value);
+  for (let i = 1; i <=  Number(formEl.elements.amount.value); i += 1) {
+    console.log(delay);
+    createPromise(i, delay).
+      then(resolve => Notiflix.Notify.success(resolve)).
+      catch(reject => Notiflix.Notify.failure(reject));
+      delay += Number(formEl.elements.step.value);
+  }
+  formEl.reset();
+}
+
+
+
+
+
